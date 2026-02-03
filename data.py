df25e9d33cddf5f488181423703f5c883f36154e
Daniel Salsbury <dgsalsbury1@gmail.com>
2026-02-03 12:04:36 -0500

    project submission

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # File with constants
+URBAN_ROUTES_URL = 'https://cnt-292b17e2-54d5-4125-8189-98ec9ea2e46b.containerhub.tripleten-services.com/'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,59 @@
+import data
+import helpers
+
+print(data.URBAN_ROUTES_URL)
+print(data.ADDRESS_FROM)
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        # Task 4: Check active server status before testing
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+    # Task 3:  Test functions for Urban Routes ordering process
+    def test_set_routes(self):
+        # Add in S8
+        print("function created for set route")
+        pass
+
+    def test_select_plan(self):
+        # Add in S8
+        print("function created for select plan")
+        pass
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("function created for fill phone number")
+        pass
+
+    def test_fill_card(self):
+        # Add in S8
+        print("function created for fill card")
+        pass
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("function created for comment for driver")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print("function created for order blanket and handkerchiefs")
+        pass
+
+    # Task 5: Adding a for loop
+    def test_order_2_ice_creams(self):
+        # Add in S8
+        print("function created for order 2 ice creams")
+        for i in range(2):
+            # Add in S8
+            pass
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print("function created for car search model appears")
+        pass
